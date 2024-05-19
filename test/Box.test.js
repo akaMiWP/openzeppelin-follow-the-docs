d7ebd4f956294d4198e7dc6bbae3ffa4063db692
akaMiWP <bangkokappdevelopment@gmail.com>
2024-05-19 15:25:05 +0700

    Updated tests

--- test/Box.test.js
+++ test/Box.test.js
@@ -4,9 +4,11 @@ const { ethers } = require("hardhat");
 describe("Box", () => {
   let Box;
   let box;
+  let owner, user;
 
   before(async () => {
     Box = await ethers.getContractFactory("Box");
+    [owner, user] = await ethers.getSigners();
   });
 
   beforeEach(async () => {
@@ -16,5 +18,15 @@ describe("Box", () => {
   it("retrieve returns a value previously stored", async () => {
     await box.store(42);
     expect((await box.retrieve()).toString()).to.equal("42");
+  });
+
+  it("store emit an event", async () => {
+    expect(await box.store("42"))
+      .to.emit(box, "ValueChanged")
+      .withArgs("42");
+  });
+
+  it("non owner cannot store a value", async () => {
+    await expect(box.connect(user).store("42")).to.be.reverted;
   });
 });
